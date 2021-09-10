7e45a5b4a0ca6c0bc29d0ec8f5966bdd2997c0f9
zlobin_ilya <qwertin1998@gmail.com>
2021-09-10 23:51:13 +0300

    Init

---
+++ server/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const cors = require("cors");
+const fs = require("fs");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/");
+  },
+});
+const uploads = multer({ storage });
+
+const port = 3000;
+
+app.use(cors());
+app.use("/static", express.static(__dirname + "/public"));
+
+app.get("/download", function (req, res) {
+  const { file } = req.query;
+  const path = `${__dirname}/public/images/`;
+  try {
+    if (fs.existsSync(path)) {
+      return res.download(`${__dirname}/public/images/${file}`);
+    }
+    return res.status(400).json({ message: "Download error" });
+  } catch (e) {
+    res.status(500).json({ message: "Download error" });
+  }
+});
+
+app.post("/upload", uploads.single("file"), function (req, res) {
+  res.send(req.file);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,178 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="pl-10 h-screen">
+    <form method="post" enctype="multipart/form-data" class="form">
+      <input type="file" ref="files" name="file" @change="uploadFile" />
+    </form>
+    <form method="post" enctype="multipart/form-data" class="form">
+      <input
+        type="file"
+        multiple="multiple"
+        ref="multipleFiles"
+        name="file"
+        @change="uploadMultipleFiles"
+      />
+    </form>
+    <div>
+      <button
+        @click="clickOnButton"
+        class="
+          flex
+          items-center
+          space-x-2
+          p-3
+          bg-blue-400
+          hover:bg-blue-500
+          transition
+          text-white
+        "
+      >
+        <span>Загрузить один файл</span>
+        <i class="ri-links-line" />
+      </button>
+    </div>
+    <div
+      ref="wrapper"
+      class="wrapper relative w-96"
+      v-if="singlefile && singlefile.name"
+    >
+      <i
+        class="
+          ri-close-line
+          absolute
+          top-2
+          right-2
+          text-3xl
+          cursor-pointer
+          hover:text-red-500
+          transition
+        "
+        @click="singlefile = null"
+      />
+      <p class="p-2 px-6 bg-blue-500 text-white">
+        Имя файла : {{ singlefile.name }}
+      </p>
+      <img :src="singlefile.link" />
+      <button
+        @click="downloadFile(singlefile.name)"
+        class="
+          flex
+          items-center
+          justify-center
+          space-x-2
+          p-1
+          bg-blue-400
+          hover:bg-blue-500
+          transition
+          text-white
+          w-full
+        "
+      >
+        <span>Скачать файл</span>
+      </button>
+    </div>
+    <div>
+      <button
+        @click="clickOnMultipleFilesButton"
+        class="
+          mt-6
+          flex
+          items-center
+          space-x-2
+          p-3
+          bg-blue-400
+          hover:bg-blue-500
+          transition
+          text-white
+        "
+      >
+        <span>Загрузить несколько файлов</span>
+        <i class="ri-links-line" />
+      </button>
+    </div>
+    <div v-if="multipleFiles.length" class="flex items-end mt-3">
+      <span class="mr-3"> Файлы: </span>
+      <span
+        v-for="(file, index) in multipleFiles"
+        :key="index"
+        class="flex items-center"
+      >
+        <a
+          class="font-bold hover:text-blue-400 transition"
+          :href="file.link"
+          @click.prevent="downloadFile(file.name)"
+          >{{ file.name }}{{ index === multipleFiles.length - 1 ? "" : ";" }}
+        </a>
+        <icon
+          class="ri-close-line hover:text-red-500 cursor-pointer transition"
+          @click="deleteLinkFromMultipleFiles(index)"
+        />
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  data: () => ({
+    singlefile: null,
+    multipleFiles: [],
+  }),
+  methods: {
+    clickOnButton() {
+      this.$refs.files.click();
+    },
+    clickOnMultipleFilesButton() {
+      this.$refs.multipleFiles.click();
+    },
+    deleteLinkFromMultipleFiles(index) {
+      this.multipleFiles = this.multipleFiles.filter(
+        (link, idx) => index !== idx
+      );
+    },
+    async uploadFile() {
+      const formData = new FormData();
+      formData.append("file", this.$refs.files.files[0]);
+      const { data } = await this.axios.post(
+        "http://localhost:3000/upload",
+        formData
+      );
+      this.singlefile = {
+        name: data.filename,
+        link: `http://localhost:3000/static/images/${data.filename}`,
+      };
+    },
+    async uploadMultipleFiles() {
+      const formData = new FormData();
+      formData.append("file", this.$refs.multipleFiles.files[0]);
+      const { data } = await this.axios.post(
+        "http://localhost:3000/upload",
+        formData
+      );
+      this.multipleFiles.push({
+        name: data.filename,
+        link: `http://localhost:3000/static/images/${data.filename}`,
+      });
+    },
+    async downloadFile(file) {
+      const res = await fetch(`http://localhost:3000/download?file=${file}`);
+      const blob = await res.blob();
+      const downloadUrl = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = downloadUrl;
+      link.download = file + ".jpg";
+      link.click();
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+img {
+  max-width: 100%;
+  height: auto;
+  object-fit: cover;
+}
+.form {
+  visibility: hidden;
 }
 </style>
